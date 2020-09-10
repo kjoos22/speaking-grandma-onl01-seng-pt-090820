654279c098c74b0dca10e8da537b47fc324f79a6
Kristian Joos <kjoos22@gmail.com>
2020-09-10 19:05:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,11 @@
 
 
 def speak_to_grandma(statement)
-  if statement.upcase != statement
-    return "HUH!? SPEAK UP, SONNY!"
-  elsif statement == "I LOVE YOU GRANDMA!"
+  if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else return "NO, NOT SINCE 1938!"
+  elsif statement == statement.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
